5d693743f29416fb89428a7a9ef7536658e3a1ab
NkechiAdeboye <nkechi.adeboye@gmail.com>
2021-01-10 22:21:45 +0000

    initial commit

---
+++ draft.py
@@ -0,0 +1,8 @@
+
+import csv
+
+with open('C:/Users/nky98/Documents/Python Project/gender_development_index.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader :
+        print(row)
+    
